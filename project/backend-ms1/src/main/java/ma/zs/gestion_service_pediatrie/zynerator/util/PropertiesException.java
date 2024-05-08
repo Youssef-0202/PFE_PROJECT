5e20b1bbcb3c7267cf929a18d6efe6e8b8474ee1
Youssef_AB <youssefaitbahssine23@gmail.com>
2024-05-08 18:20:28 +0100

    m3

--- project/backend-ms1/src/main/java/ma/zs/gestion_service_pediatrie/zynerator/util/PropertiesException.java
+++
@@ -1,7 +0,0 @@
-package ma.zs.gestion_service_pediatrie.zynerator.util;
-
-public class PropertiesException extends Exception {
-    public PropertiesException(String message) {
-        super(message);
-    }
-}
